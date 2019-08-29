4a70f884d22f9795d5edde651b042b16c743d08b
LilPrice-Code <littleprice002@gmail.com>
2019-08-29 10:28:13 -0500

    done

--- GroupProject.py
+++ GroupProject.py
@@ -4,15 +4,18 @@ def listTask():
     for i in range (0, len(list)):
         print(f"{i+1}.{list[i]}")
 
-
 def addTask():
     listTask()
-    taskInput= input("Enter new task to add:  ")
-    list.append(taskInput)
+    add = 0
+    while add == 0:
+        taskInput= input("Enter new task to add\nor Press x to go back: ")
+        if taskInput == "x" or taskInput == "X":
+            add +=1
+        else:
+            list.append(taskInput)
+            add +=1
 
 def deleteTask():
     listTask()
     deleteInput=input("Which task would you like to delete? ")
     list.pop(int(deleteInput)-1)
-    listTask()
-deleteTask()
--- menu.py
+++ menu.py
@@ -3,8 +3,8 @@ from GroupProject import addTask
 from GroupProject import deleteTask
 menuExit = 0
 while menuExit == 0:
-    user = int(input("Congratulations! You're running [YOUR NAME]'s Task List program.\nWhat would you like to do next?\n1. List all tasks.\n"
-                     "2.Add a task to the list.\n3. Delete a task.\n0. To quit the program\n"))
+    user = int(input("Congratulations! You're running [YOUR NAME]'s Task List program.\nWhat would you like to do next?\n1.List all tasks.\n"
+                     "2.Add a task to the list.\n3.Delete a task.\n0.To quit the program\n"))
     if user == 0:
         menuExit +=1
     elif user == 1:
@@ -13,6 +13,6 @@ while menuExit == 0:
     elif user == 2:
         addTask()
     elif user == 3:
-        print("In Progress")
+        deleteTask()
     else:
         print("INVALID ANSWER")
