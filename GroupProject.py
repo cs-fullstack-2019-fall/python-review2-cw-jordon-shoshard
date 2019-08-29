d6530df197a9bf1855dac488f03da62fd29f8c5e
jordondoug2019 <jdouglas2015@gmail.com>
2019-08-29 09:39:28 -0500

    Started List

---
+++ GroupProject.py
@@ -0,0 +1,9 @@
+list=["Cut the grass"," Clean the fridge"," Clean my room", " Wash clothes","Make dinner","Wash dishes"]
+def listTask():
+    for i in range ():
+        print(array[i])
+
+def addTask():
+    listTask()
+    taskInput= input("Enter new task to add:  ")
+    listTask
