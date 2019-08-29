55881e15181f451c5953165e2ea7c6fbb33b5be9
LilPrice-Code <littleprice002@gmail.com>
2019-08-29 11:20:52 -0500

    Full completed

--- menu.py
+++ menu.py
@@ -3,7 +3,7 @@ from GroupProject import addTask
 from GroupProject import deleteTask
 menuExit = 0
 while menuExit == 0:
-    user = int(input("Congratulations! You're running [YOUR NAME]'s Task List program.\nWhat would you like to do next?\n1.List all tasks.\n"
+    user = int(input("Congratulations! You're running Jordan-Shoshard's Task List program.\nWhat would you like to do next?\n1.List all tasks.\n"
                      "2.Add a task to the list.\n3.Delete a task.\n0.To quit the program\n"))
     if user == 0:
         menuExit +=1
