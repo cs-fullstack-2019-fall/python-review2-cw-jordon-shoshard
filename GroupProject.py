4e7276ddc8c6941c2b40b041f59c72d69f749caf
jordondoug2019 <jdouglas2015@gmail.com>
2019-08-29 09:45:56 -0500

    Jordons mess up

--- GroupProject.py
+++ GroupProject.py
@@ -1,9 +1,9 @@
 list=["Cut the grass"," Clean the fridge"," Clean my room", " Wash clothes","Make dinner","Wash dishes"]
-def listTask():
-    for i in range ():
-        print(array[i])
 
+def listTask():
+     for i in range( ):
+        print(list[i]
 def addTask():
     listTask()
     taskInput= input("Enter new task to add:  ")
-    listTask
+    list.append(taskInput)
