9736f4cbca89860c8972ffd598e80cad429dfddb
LilPrice-Code <littleprice002@gmail.com>
2019-08-29 10:39:07 -0500

    done

--- GroupProject.py
+++ GroupProject.py
@@ -8,7 +8,7 @@ def addTask():
     listTask()
     add = 0
     while add == 0:
-        taskInput= input("Enter new task to add\nor Press x to go back: ")
+        taskInput= input("Enter new task to add\nor Enter 'x' to go back: ")
         if taskInput == "x" or taskInput == "X":
             add +=1
         else:
@@ -17,5 +17,15 @@ def addTask():
 
 def deleteTask():
     listTask()
-    deleteInput=input("Which task would you like to delete? ")
-    list.pop(int(deleteInput)-1)
+    delete = 0
+    while delete == 0:
+        deleteInput=input("Which task would you like to delete?\nor Enter 'x' to go back: ")
+        if deleteInput == "x" or deleteInput == "X":
+            delete +=1
+        elif int(deleteInput) > 0 and int(deleteInput) <= len(list):
+            list.pop(int(deleteInput)-1)
+            delete +=1
+        else:
+            print("INVALID ANSWER")
+
+
